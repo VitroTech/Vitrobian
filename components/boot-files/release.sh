d690de1beee6c7b70d0bf29365ffca941c077269
Piotr Kleinschmidt <piotr.kleinschmidt@3mdeb.com>
2020-11-20 16:36:19 +0100

    components/boot-files/release.sh: fix error information
    Signed-off-by: Piotr Kleinschmidt <piotr.kleinschmidt@3mdeb.com>

--- components/boot-files/release.sh
+++ components/boot-files/release.sh
@@ -6,5 +6,5 @@ TARBALL="${OUT_DIR}_${VITROBIAN_VERSION}.tar.gz"
 
 # upload package
 [ -z $AWS_PROFILE ] && echo "AWS_PROFILE not given" && exit
-[ -z $AWS_BUCKET ] && echo "AWS_PROFILE not given" && exit
+[ -z $AWS_BUCKET ] && echo "AWS_BUCKET not given" && exit
 aws s3 cp $TARBALL s3://$AWS_BUCKET --profile $AWS_PROFILE --acl public-read
